6347b3ac6c03c1e69cfab1915fe7ec2eabd6cd54
thathireddypurushotham2000@gmail.com <thathireddypurushotham2000@gmail.com>
2021-06-13 09:25:39 +0530

    aa

--- Library_App/views.py
+++ Library_App/views.py
@@ -141,64 +141,34 @@ def Books_AvailF(rq):
 	return render(rq,"html/Books_Avail.html",{'t':e2,'t1':e})
 @login_required
 def sendrequest(rq,id):
-	k= Books_Avail.objects.get(id=id)
+	m= Books_Avail.objects.get(id=id)
 	if rq.method=="POST":
 		a=rq.POST['Book_name']
 		c=rq.POST['Book_author']
-		b=rq.POST['browser']
-		if b=='Book_Retrun':
-			notes=st_admin_data.objects.filter(Book_name=a)
-			for i in notes:
-				if i.Book_author==c and i.issue_status==1:
+		
+		
+		print(a)
+		book_name=[]
+		e=Books_AvailForm(rq.POST)
+		notes=Books_Avail.objects.all()
+		for i in notes:
+			if i.Book_name==a:
+				k=i.Book_count
+		print(book_name)
 
-					i.issue_status='3'
-					showtime = strftime("%Y-%m-%d")
-					print(showtime)
-					i.Return_Date=showtime
-					l=str(showtime).split("-")
-					l1=str(i.Expire_date).split("-")
-					d1 = date(int(l[0]), int(l[1]), int(l[2]))
-					d0 = date(int(l1[0]), int(l1[1]), int(l1[2]))
-					delta = d1 - d0
-					i.Fine=delta.days*2
-					if i.Fine>0:
-						subject='Fine Info'
-						body="Hi +{{rq.user.username}}+ Your fine amount is "
-						receiver=rq.user.email
-						sender=settings.EMAIL_HOST_USER
-						t=EmailMessage(subject,body,sender,[receiver])
-						t.send()
-						return httpResponse(" Successfully_Sent")
-
-
-
-					print(delta.days)
-
-					print(l)
-					i.save()
-					k.save()
-		else:
-			print(a)
-			book_name=[]
-			e=Books_AvailForm(rq.POST)
-			notes=Books_Avail.objects.all()
-			for i in notes:
-				if i.Book_name==a:
-					k=i.Book_count
-			print(book_name)
-
-			if e.is_valid():
-				q=e.save(commit=False)
-				q.uid_id=rq.user.id
-				q.Book_count=k
-				q.Name=rq.user.username
-				q.Rg_No=rq.user.Rg_No
-				q.Branch=rq.user.Branch
-				q.save()
+		if e.is_valid():
+			q=e.save(commit=False)
+			q.uid_id=rq.user.id
+			q.Book_count=k
+			q.Name=rq.user.username
+			q.Rg_No=rq.user.Rg_No
+			q.Branch=rq.user.Branch
+			q.save()
 
 		
-	e=Books_AvailForm(instance=k)
+	e=Books_AvailForm(instance=m)
 	return render(rq,"html/sendrequest.html",{'t':e})
+
 
 
 
@@ -273,10 +243,45 @@ def rejectadmin(req,id):
 	rc.save()
 	return redirect('/viewn')
 
-def books_return(request):
-	rc=st_admin_data.objects.all()
-	print(rc)
-	return render(request,'html/Books_return.html',{'rc':rc})
+def books_return(request,id):
+
+	i=st_admin_data.objects.get(id=id)
+	if i.issue_status==1:
+		i.issue_status='3'
+		k=Books_Avail.objects.all()
+		for j in k:
+			if j.Book_name==i.Book_name and j.Book_author==i.Book_author:
+				j.Book_count=j.Book_count+1
+				j.save()
+			
+		showtime = strftime("%Y-%m-%d")
+		print(showtime)
+		i.Return_Date=showtime
+		l=str(showtime).split("-")
+		l1=str(i.Expire_date).split("-")
+		d1 = date(int(l[0]), int(l[1]), int(l[2]))
+		d0 = date(int(l1[0]), int(l1[1]), int(l1[2]))
+		delta = d1 - d0
+		
+		i.Fine=delta.days*2
+		if i.Fine>0:
+			subject='Fine Info'
+			body="Hi +{{request.user.username}}+ Your fine amount is "
+			receiver=request.user.email
+			sender=settings.EMAIL_HOST_USER
+			t=EmailMessage(subject,body,sender,[receiver])
+			t.send()
+						# return httpResponse(" Successfully_Sent")
+
+
+
+			print(delta.days)
+
+			print(l)
+			i.save()
+
+
+	return redirect('/books_st_have')
 
 def issue_book(request):
 	rc=st_admin_data.objects.all()
